6f2dc24ecb7fc04c98271a8e4c7c3d8f30826a5c
Your Name <mailsonccardoso@gmail.com>
2025-12-26 01:21:28 -0300

    botoes

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -189,8 +189,8 @@ const ResultPage = () => {
               </Button>
             </Link>
             <Button
-              variant="outline"
-              className="w-full h-12"
+              variant="navy"
+              className="w-full h-12 shadow-md"
               onClick={() => setShowDetails(!showDetails)}
             >
               <BarChart3 className="w-5 h-5 mr-2" />
